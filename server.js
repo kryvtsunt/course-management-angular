77aae85a8a04ed75cc645e5abab3a0181249c36f
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-06-19 21:43:59 -0400

    updated to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tk-course-management-angular'));
+app.use(express.static('/dist/tk-course-management-angular'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/tk-course-management-angular/index.html'));
+res.sendFile(path.join('/dist/tk-course-management-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
